1bf588c0cefb95b88b9c041b6f28619211545c92
abasanwan <abasanwan@gmail.com>
2022-09-17 15:29:58 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,30 @@
 #include "main.h"
 
 /**
- * print_square - prints hashes squares.
- * @size: size of the square.
- * Return: no return.
+ * print_square - prints a square
+ * @size: the size of the square
+ *
  */
+
 void print_square(int size)
+
 {
 	int x, y;
 
-	for (x = 0; x < size; x++)
+	if (size > 0)
 	{
-		for (y = 0; y < size; y++)
+		for (x = 0; x < size; x++)
 		{
-			_putchar(35);
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
 		}
-		if (x != size - 1)
-			_putchar('\n');
+	}
+	else
 	{
-	_putchar('\n');
+		_putchar(10);
+	}
+
 }
