1965c8d51e294f533826e145dec903788206879b
abasanwan <abasanwan@gmail.com>
2022-09-17 06:41:37 -0700

    shortest distance

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,17 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints diagonal line n times.
- * @n: times diagonal line is printed.
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
  * Return: no return.
  */
-void print_diagonal(int n)
+void print_line(int n)
 {
-	int x, y;
+	int i;
 
-	for (x = 0; x < n; x++)
+	for (i = 0; i < n; i++)
 	{
-		for (y = 0; y < x; y++)
-		{
-			_putchar(' ');
-		}
-		_putchar(92);
-		if (x < (n - 1))
-			_putchar('\n');
+		_putchar(95);
 	}
 	_putchar('\n');
 }
